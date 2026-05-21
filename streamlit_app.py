afccc8341517f6cd485be1f712e7e47026f34e19
Lowfatm1ilk <shawndaigogo@gmail.com>
2026-05-21 22:48:15 +0000

    yay

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,54 @@
 import streamlit as st
 from openai import OpenAI
 import json
+def text_to_morse(text):
+    morse = []
+
+    for char in text.upper():
+        if char in MORSE_CODE:
+            morse.append(MORSE_CODE[char])
+
+    return " ".join(morse)
+
+st.subheader("Translate this Morse code:")
+
+st.code(st.session_state.morse)
 system_prompt = """
-You are gonna give the user a stock that has the biggest percent positive change within this week
-Respond in a json in the following format: 
-{Name: stock abbreviation,
-percent change: +%,
-dollar change: +$,
-current value: #,
-daterange: mm/dd/yyyy -mm/dd/yyyy
-}
+You're gonna generate one simmple sentence in morse code no punctuation and under 6 words is optimal.
 """
+user_answer = st.text_input("Type the English translation:")
 client = OpenAI(api_key=st.secrets['json'] )
 chat_history = [
     {"role": "system", "content": system_prompt}
-
-
 ]
-
+MorseCode = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+}
 response = client.chat.completions.create(
             response_format={'type':'json_object'},
             model='gpt-4o',
@@ -25,3 +56,10 @@ response = client.chat.completions.create(
         )
 
 st.write(json.loads(response.choices[0].message.content))
+if st.button("Submit"):
+    if user_answer.upper().strip() == st.session_state.sentence:
+        st.success("Correct!")
+    else:
+        st.error(f"Incorrect. The answer was: {st.session_state.sentence}")
+
+
