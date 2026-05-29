a147586c3a493a4f63f58fad2578ed4c66af5ca2
Lowfatm1ilk <shawndaigogo@gmail.com>
2026-05-29 00:18:50 +0000

    i

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ for i in range(0,len(k),4):
     col2.header(MorseCode[k[i+1]])
     col2.subheader(k[i+1])
     i
-    if i == 23:
+    if i >= 23:
         break
 
     col3.header(MorseCode[k[i+2]])
